1159b009bcb25bb42a92dd7bb25f3a8f6483f0a1
Augusto Silva <augustonicolassilva@gmail.com>
2022-05-12 23:36:30 -0300

    navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return(
+        <nav>
+            <h1>E-commerce</h1>
+            <ul>
+                <li>Hombre</li>
+                <li>Mujer</li>
+                <li>Niños</li>
+            </ul>
+            <button type="button">Login</button>
+        </nav>
+    )
+}
+export default NavBar
